a066268d1ca0408445ad29d27f3e647320fa124f
Catalin Iovita <60446603+cataliniovita@users.noreply.github.com>
2024-09-27 00:49:31 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -56,7 +56,7 @@ const App = () => {
             <Education />
           </div>
         </div>
-        <Certificates />
+        <Achievements />
         <div className={`bg-primary ${styles.flexCenter} ${styles.paddingX}`}>
           <div className={`${styles.boxWidth}`}>
             <Projects />
