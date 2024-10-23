7668ceda3c42872fb51a2c084eaaca2e240dcadb
Catalin Iovita <60446603+cataliniovita@users.noreply.github.com>
2024-10-23 17:53:54 +0300

    Update Education.jsx

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -42,14 +42,6 @@ const FeatureCard = ({
       <p className="font-poppins font-normal text-dimWhite text-[14px] leading-[30px] mb-1">
         {duration}
       </p>
-      <p className="font-poppins font-normal text-dimWhite text-[16px] leading-[30px] mb-1">   
-      ● {content1}
-      </p>
-      {content2 && (
-        <p className="font-poppins font-normal text-dimWhite text-[16px] leading-[30px] mb-1">     
-      ● {content2}
-        </p>
-      )}
     </div>
   </div>
 );
